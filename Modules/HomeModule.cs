892b9f81e799273355c7de938202da9db29d2160
ian Wilcox <ian Wilcox>
2016-09-30 15:31:46 -0700

    index can list out all and has forms that work for both

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -21,7 +21,33 @@ namespace BandTracker
         return View["index.cshtml", model];
       };
 
+      Post["/add_band"] = _ =>
+      {
+        string bandName = Request.Form["bandName"];
+        Band band = new Band(bandName);
+        band.Save();
 
+        List<Band> bandList = Band.GetAll();
+        List<Venue> venueList = Venue.GetAll();
+        Dictionary<string, object> model = new Dictionary<string, object> {};
+        model.Add("bandList", bandList);
+        model.Add("venueList", venueList);
+        return View["index.cshtml", model];
+      };
+
+      Post["/add_venue"] = _ =>
+      {
+        string venueName = Request.Form["venueName"];
+        Venue venue = new Venue(venueName);
+        venue.Save();
+
+        List<Band> bandList = Band.GetAll();
+        List<Venue> venueList = Venue.GetAll();
+        Dictionary<string, object> model = new Dictionary<string, object> {};
+        model.Add("bandList", bandList);
+        model.Add("venueList", venueList);
+        return View["index.cshtml", model];
+      };
     }
   }
 }
